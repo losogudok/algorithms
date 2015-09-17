d90764ef46912aea9df24cb7aec142e5301c2eb4
Andrey Kostin <losogudok@yandex.ru>
2015-09-17 23:36:47 +0300

    binary tree description

---
+++ data_structures/binary_search_tree/index.js
@@ -0,0 +1,38 @@
+function BinarySearchTree() {
+    this._root = null;
+}
+
+BinarySearchTree.prototype = {
+    constructor: BinarySearchTree,
+    minimum() {
+
+    },
+    maximum() {
+
+    },
+    successor() {
+
+    },
+    predecessor() {
+
+    },
+    insert(value){
+
+    },
+    remove: function(value){
+
+    },
+    search: function(value){
+
+    },
+    size: function(){
+
+    },
+
+    toArray: function(){
+    },
+
+    toString: function(){
+    }
+
+};
